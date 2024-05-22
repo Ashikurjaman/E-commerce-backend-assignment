cad5b999ef8ee8162872acc75274c5f933212fc7
unknown <ashikbur007@gmail.com>
2024-05-22 19:38:29 +0600

    create product router for add product in database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { ProductRoute } from './app/Modules/Product/products.router';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/products', ProductRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
